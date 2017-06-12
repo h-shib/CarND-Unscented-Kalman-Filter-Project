84f3f9fc3f15fcadc0da8affe3e46f8b4cd34ca0
h-shib <hiraku.shibuya888@gmail.com>
2017-06-12 19:48:29 +0900

    implement UpdateRadar function

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -199,25 +199,25 @@ void UKF::Prediction(double delta_t) {
   * predict mean and covariance
   */
   // set weights
-  VectorXd weights = VectorXd(2*n_aug_+1);
+  weights_ = VectorXd(2*n_aug_+1);
   for (int i = 0; i < 2*n_aug_+1; i++) {
     if (i == 0) {
-      weights[i] = lambda_ / (lambda_ + n_aug_);
+      weights_[i] = lambda_ / (lambda_ + n_aug_);
     } else {
-      weights[i] = 0.5 / (lambda_ + n_aug_);
+      weights_[i] = 0.5 / (lambda_ + n_aug_);
     }
   }
 
   // predict state mean
   x_.fill(0.0);
   for (int i = 0; i < 2*n_aug_+1; i++) {
-    x_ += weights[i] * Xsig_pred_.col(i);
+    x_ += weights_[i] * Xsig_pred_.col(i);
   }
 
   // predict state covariance matrix
   P_.fill(0.0);
   for (int i = 0; i < 2*n_aug_+1; i++) {
-    P_ += weights[i] * (Xsig_pred_.col(i) - x_) * (Xsig_pred_.col(i) - x_).transpose();
+    P_ += weights_[i] * (Xsig_pred_.col(i) - x_) * (Xsig_pred_.col(i) - x_).transpose();
   }
 }
 
@@ -249,4 +249,69 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
 
   You'll also need to calculate the radar NIS.
   */
+  // set measurement dimension
+  int n_z = 3;
+
+  // matrix for sigma points in measurement space
+  MatrixXd Zsig = MatrixXd(n_z, 2*n_aug_+1);
+
+  // mean prediction
+  VectorXd z_pred = VectorXd(n_z);
+
+  // measurement covariance matrix
+  MatrixXd S = MatrixXd(n_z, n_z);
+
+  // measurement
+  VectorXd z;
+  z << meas_package.raw_measurements_[0],
+       meas_package.raw_measurements_[1],
+       meas_package.raw_measurements_[2];
+
+  for (int i = 0; i < 2* n_aug_+1; i++) {
+    float px   = Xsig_pred_(0, i);
+    float py   = Xsig_pred_(1, i);
+    float v    = Xsig_pred_(2, i);
+    float yaw  = Xsig_pred_(3, i);
+    float yawd = Xsig_pred_(4, i);
+
+    float ro = sqrt(px*px + py*py);
+    float theta = atan(py/px);
+    float ro_dot = (px*cos(yaw)*v + py*sin(yaw)*v) / ro;
+
+    Zsig(0, i) = ro;
+    Zsig(1, i) = theta;
+    Zsig(2, i) = ro_dot;
+  }
+
+  // calculate mean
+  z_pred.fill(0.0);
+  for (int i = 0; i < 2*n_aug_+1; i++) {
+    z_pred = z_pred + weights_[i] * Zsig.col(i);
+  }
+
+  // calculate measurement covariance
+  S.fill(0.0);
+  for (int i = 0; i < 2*n_aug_+1; i++) {
+    S += weights_[i] * (Zsig.col(i) - z_pred) * (Zsig.col(i) - z_pred).transpose();
+  }
+  MatrixXd R = MatrixXd(n_z, n_z);
+  R << std_radr_*std_radr_, 0, 0,
+       0, std_radphi_*std_radphi_, 0,
+       0, 0, std_radrd_*std_radrd_;
+  S += R;
+
+  // cross correlation matrix
+  MatrixXd Tc = MatrixXd(n_x_, n_z);
+
+  Tc.fill(0.0);
+  for (int i = 0; i < 2*n_aug_+1; i++) {
+    Tc += weights_[i] * (Xsig_pred_.col(i) - x_) * (Zsig.col(i) - z_pred).transpose();
+  }
+
+  MatrixXd K = MatrixXd(n_x_, n_z);
+  K = Tc * S.inverse();
+
+  x_ += K * (z - z_pred);
+  P_ += -K * S * K.transpose();
+
 }
